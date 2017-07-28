933920767a248b7e1efd2cfa3d1f17cbc90dc4fe
nlj1617 <nlj1617@163.com>
2017-07-28 17:11:43 +0800

    JD
    0728

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+window.onload=function(){
+	var closebtn=document.getElementById('closebtn');
+	var adBox=document.getElementById('adBox');
+	closebtn.onclick=function(){
+       adBox.style.display="none";
+    }    
+}
